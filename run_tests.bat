55a329053e4590ed6260186e100d48cbf13a7c37
Fardin957 <fardinfaruk2001@gmail.com>
2026-04-11 17:08:59 +0600

    add new reportes

--- run_tests.bat
+++ run_tests.bat
@@ -1,32 +1,13 @@
 @echo off
 mkdir reports 2>nul
 
-echo Running Login Test...
-call maestro test e2e/logintestcases/login_with_valid_cred.yaml -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/01_login_report.html
+echo Running All Tests...
+call maestro test e2e/logintestcases/ -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/01_login_report.html
+call maestro test e2e/homescreentestcase/ -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/02_home_report.html
+call maestro test e2e/notificationtestcase/ -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/03_notification_report.html
+call maestro test e2e/topuptestcase/ -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/04_topup_report.html
+call maestro test e2e/transactiontestcase/ -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/05_transaction_report.html
+call maestro test e2e/sendcryptocashout/ -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/06_sendcrypto_report.html
 
-echo Running Homescreen Test...
-call maestro test e2e/homescreentestcase/homescreen.yaml -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/02_homescreen_report.html
-
-echo Running Notification Test...
-call maestro test e2e/notificationtestcase/notification.yaml -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/03_notification_report.html
-
-echo Running Crypto Test...
-call maestro test e2e/topuptestcase/cryptotestcase.yaml -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/04_cryptotestcase_report.html
-
-echo Running Crypto Request Test...
-call maestro test e2e/topuptestcase/cryptorequest.yaml -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/05_cryptorequest_report.html
-
-echo Running Zigift Card Test...
-call maestro test e2e/topuptestcase/zigiftcard.yaml -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/06_zigiftcard_report.html
-
-echo Running Transaction Test...
-call maestro test e2e/transactiontestcase/transaction.yaml -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/07_transaction_report.html
-
-echo Running Crypto Cashout Test...
-call maestro test e2e/sendcryptocashout/cryptocashout.yaml -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/08_cryptocashout_report.html
-
-echo Running Wallets Send Money Test...
-call maestro test e2e/sendcryptocashout/wallets_sendmoney.yaml -e Mobile_Number=1617539764 -e Password=Password100@ --format html --output reports/09_wallets_report.html
-
-echo ✅ All Tests Completed!
+echo All Tests Completed!
 pause
